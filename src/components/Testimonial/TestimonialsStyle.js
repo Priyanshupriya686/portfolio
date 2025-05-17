10e9d58c6c50128990b02e3b2d16d664fa02ae1c
d3basmi1a <d3basmi1a@gmail.com>
2025-05-17 19:39:02 +0530

    hello

--- src/components/Testimonial/TestimonialsStyle.js
+++ src/components/Testimonial/TestimonialsStyle.js
@@ -7,7 +7,7 @@ export const Container = styled.div`
   position: relative;
   z-index: 1;
   align-items: center;
-  padding: 0px 0 0px 0; /* Reduced top and bottom padding */
+  padding: 0px 0 0px 0; 
   @media (max-width: 960px) {
     padding: 10px 0;
   }
@@ -21,7 +21,7 @@ export const Wrapper = styled.div`
   flex-direction: column;
   width: 100%;
   max-width: 1350px;
-  padding: 10px 0; /* Reduced vertical padding */
+  padding: 10px 0; 
   gap: 12px;
   @media (max-width: 960px) {
     flex-direction: column;
@@ -33,10 +33,10 @@ export const Title = styled.div`
   font-size: 42px;
   text-align: center;
   font-weight: 600;
-  margin-top: 12px;  /* Reduced from 20px */
+  margin-top: 20px;  
   color: ${({ theme }) => theme.text_primary};
   @media (max-width: 768px) {
-    margin-top: 12px;
+    margin-top: 0;
     font-size: 32px;
   }
 `;
@@ -46,9 +46,8 @@ export const Desc = styled.div`
   text-align: center;
   max-width: 600px;
   color: ${({ theme }) => theme.text_secondary};
-  margin-top: 6px; /* Added small space */
+  margin-top: -40px; 
   @media (max-width: 768px) {
-    margin-top: 6px;
     font-size: 16px;
   }
 `;
